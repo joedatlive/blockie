0674e3af87c26ed2286a92815b5311398b16e770
joedatlive <joed@live.com>
2018-07-16 14:52:25 -0700

    cleaned up older versions and testws by moving to archive folder

--- block/block.go
+++ block/block.go
@@ -4,6 +4,46 @@ import "time"
 import "fmt"
 import "crypto/sha256"
 import "encoding/hex"
+import "net/http"
+import "google.golang.org/appengine"
+import "encoding/json"
+
+type Block struct {
+	Hash string
+	Index int
+	Timestamp time.Time
+	PreviousHash []byte
+	Data string
+}
+
+//http handler below
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintln(w, "Best Blockie Ever!") // Response to request
+
+	//adding block creation logic
+	ts := timeStamp()
+	previousHash := []byte("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
+	data := "abc"
+	index := 1
+	
+	// pass in this block's data to hasher and return hash
+	blockhash := hasher(index, previousHash, ts, data)
+
+	block := Block{
+		Hash: blockhash,
+		Index:index,
+		Timestamp: ts,
+		PreviousHash: previousHash,
+		Data: data,
+	}
+
+	b,err := json.Marshal(block)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintln(w, string(b))
+  }
 
 func timeStamp() time.Time {
 	ts := time.Now().UTC()
@@ -21,27 +61,12 @@ func hasher(index int, previousHash []byte, timeStamp time.Time, data string) st
 	return string(sha256_hash)
 }
 
-// a fuction which takes input and creates a block
-
 func main() {
 	//set variables
-	ts := timeStamp()
-	previousHash := []byte("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
-	data := "abc"
-	index := 1
 	
-	// pass in this block's data to hasher and retrun hash
-	block := hasher(index, previousHash, ts, data)
-	
-	//print block data
-	fmt.Println("Block: ", block)
-	fmt.Println("Block data")
-	fmt.Println("Index: ", index)
-	fmt.Println("Previous hash: ", previousHash)
-	fmt.Println("Timestamp: ", ts.Format(time.RFC3339))
-	fmt.Println("Data: ", data)
-	fmt.Println("Version: Block.go ver .1")
+
+	//starts web server 
+	http.HandleFunc("/block", indexHandler) // set endpoint handler
+		appengine.Main() // starts the server to receive requests
 	
 }
-
-//func Block (string h) int index, time.Time ts, string data, string previousHash
