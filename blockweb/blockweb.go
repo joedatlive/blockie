2271dfa0526b1072676e9c258907dc1e84db99b8
joedatlive <joed@live.com>
2018-07-16 14:25:59 -0700

    add block as a struc and then convert to json

--- blockweb/blockweb.go
+++ blockweb/blockweb.go
@@ -6,6 +6,15 @@ import "crypto/sha256"
 import "encoding/hex"
 import "net/http"
 import "google.golang.org/appengine"
+import "encoding/json"
+
+type Block struct {
+	Hash string
+	Index int
+	Timestamp time.Time
+	PreviousHash []byte
+	Data string
+}
 
 //http handler below
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,29 +37,33 @@ func hasher(index int, previousHash []byte, timeStamp time.Time, data string) st
 	return string(sha256_hash)
 }
 
-// a fuction which takes input and creates a block
-
 func main() {
-	//starts web server 
-	http.HandleFunc("/blockweb", indexHandler) // set endpoint handler
-		appengine.Main() // starts the server to receive requests
-	
 	//set variables
 	ts := timeStamp()
 	previousHash := []byte("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
 	data := "abc"
 	index := 1
 	
-	// pass in this block's data to hasher and retrun hash
-	block := hasher(index, previousHash, ts, data)
-	
-	//print block data
-	fmt.Println("Block: ", block)
-	fmt.Println("Block data")
-	fmt.Println("Index: ", index)
-	fmt.Println("Previous hash: ", previousHash)
-	fmt.Println("Timestamp: ", ts.Format(time.RFC3339))
-	fmt.Println("Data: ", data)
-	fmt.Println("Version: Block.go ver .1")
+	// pass in this block's data to hasher and return hash
+	blockhash := hasher(index, previousHash, ts, data)
+
+	block := Block{
+		Hash: blockhash,
+		Index:1,
+		Timestamp: timeStamp(),
+		PreviousHash: []byte("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),
+		Data: "abc",
+	}
+
+	b,err := json.Marshal(block)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
+
+	//starts web server 
+	http.HandleFunc("/blockweb", indexHandler) // set endpoint handler
+		appengine.Main() // starts the server to receive requests
 	
 }
